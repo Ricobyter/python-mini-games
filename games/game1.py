53ab5f6b537474937eb654aa4d9abf4d913bdef5
Dhruv <dhruvanandsingh2004@gmail.com>
2024-02-04 14:22:54 +0530

    completed game1

--- games/game1.py
+++ games/game1.py
@@ -1,35 +1,54 @@
 import random
 
+
 def roll():
     min_value = 1
     max_value = 6
-    roll = random.randint(min_value, max_value) 
+    roll = random.randint(min_value, max_value)
 
     return roll
 
+
 while True:
-    players = input("Enter the number of players(2-4): ")
+    players = input("Enter the number of players (2 - 4): ")
     if players.isdigit():
         players = int(players)
-        if 2<= players <= 4:
+        if 2 <= players <= 4:
             break
         else:
-            print("Invalid number.Enter a number between 2 and 4")
-    
+            print("Must be between 2 - 4 players.")
     else:
-        print("Invalid entry. Enter a valid number")
+        print("Invalid, try again.")
 
 max_score = 50
-
-player_scores = [0 for _ in range(len(players))]
+player_scores = [0 for _ in range(players)]
 
 while max(player_scores) < max_score:
-    should_roll  = input("would you like to rolll (y): ")
-    if should_roll.lower() != "y":
-        break
+    for player_idx in range(players):
+        print("\nPlayer number", player_idx + 1, "turn has just started!")
+        print("Your total score is:", player_scores[player_idx], "\n")
+        current_score = 0
 
-    value = roll()
+        while True:
+            should_roll = input("Would you like to roll (y)? ")
+            if should_roll.lower() != "y":
+                break
 
+            value = roll()
+            if value == 1:
+                print("You rolled a 1! Turn done!")
+                current_score = 0
+                break
+            else:
+                current_score += value
+                print("You rolled a:", value)
 
+            print("Your score is:", current_score)
 
+        player_scores[player_idx] += current_score
+        print("Your total score is:", player_scores[player_idx])
 
+max_score = max(player_scores)
+winning_idx = player_scores.index(max_score)
+print("Player number", winning_idx + 1,
+      "is the winner with a score of:", max_score)
